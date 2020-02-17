a4c1b028abf584ecc754abd8378cca63323357c7
Raizer58 <Raizer9058w@gmail.com>
2020-02-17 20:47:49 +0300

    init: start basic-js task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,6 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(arg) {
+  return typeof arg == "string" && parseFloat(arg) < 15 && parseFloat(arg) > 0 ? Math.ceil((Math.log(MODERN_ACTIVITY/parseFloat(arg)))/(0.693/HALF_LIFE_PERIOD)) : false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arg) {
+  let countCat = 0;
+  arg.forEach(arg_1 => arg_1.forEach(arg_2 => countCat += arg_2 == "^^" ? 1 : 0));
+  return countCat;
 };
