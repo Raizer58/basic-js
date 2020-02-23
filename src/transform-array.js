2878eaf0c76edd4116b26a38f69e17e12c8f4954
Raizer58 <Raizer9058w@gmail.com>
2020-02-23 23:31:13 +0300

    feat: add decision transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,10 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr ) {
+  let arrayTransform = [];
+  arr.forEach((arg, index)=>
+    arg == "--discard-next"? false:
+    arg == "--discard-prev"? arrayTransform.pop(arg):
+    arg == "--double-next" ? index !== arr.length-1? arrayTransform.push(arr[index+1]):false:
+    arg == "--double-prev" ? index > 0? arrayTransform.push(arr[index-1]):false:
+    arr[index-1] !== "--discard-next"? arrayTransform.push(arg):false, [])
+  return arrayTransform;
 };
