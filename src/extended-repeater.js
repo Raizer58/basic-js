0dc7f3fb2e56893c400dd43f7fe43fbf3f609bfb
Raizer58 <Raizer9058w@gmail.com>
2020-02-24 22:07:55 +0300

    feat: add decision extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,32 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let repeatStr = Array(options.repeatTimes);
+
+    if (options.addition !== undefined){
+         
+        if (options.additionRepeatTimes){
+            let addRepeateTimes = Array(options.additionRepeatTimes);
+            addRepeateTimes.fill(String(options.addition));
+  
+            if (options.additionSeparator){
+              str += addRepeateTimes.join(options.additionSeparator);
+            }
+            else {
+              str += addRepeateTimes.join("|");
+            };
+        }
+        else{
+            str += options.addition
+        }
+    };
+    
+    repeatStr.fill(String(str));
+
+    if (options.separator){
+       return repeatStr.join(options.separator);
+    }
+    else {
+      return repeatStr.join("+");
+    };
 };
+
   
